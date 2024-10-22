5f18f1e95a8c8c29b7dca2a5a098ff48961ecde6
Martin <martin_lenart@icloud.com>
2024-10-22 14:58:54 +0200

    singleton

---
+++ M3_08_05a/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace BOOPM3_08_05a
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //NON singleton pattern
+            System.Console.WriteLine("NON Singleton pattern");
+            System.Console.WriteLine(new csPasswordManager().Password1);
+            System.Console.WriteLine(new csPasswordManager().Password2);
+
+            //some other part of your code
+            System.Console.WriteLine(new csPasswordManager().Password1);
+            System.Console.WriteLine(new csPasswordManager().Password2);
+
+            //yet some other part of your code
+            System.Console.WriteLine(new csPasswordManager().Password1);
+            System.Console.WriteLine(new csPasswordManager().Password2);
+
+            //WITH singleton pattern
+            System.Console.WriteLine("\nWITH Singleton pattern");
+
+            System.Console.WriteLine(csPasswordManager.Instance.Password1);
+            System.Console.WriteLine(csPasswordManager.Instance.Password2);
+
+            System.Console.WriteLine(csPasswordManager.Instance.Password1);
+            System.Console.WriteLine(csPasswordManager.Instance.Password2);
+
+            //some other part of your code
+            System.Console.WriteLine(csPasswordManager.Instance.Password1);
+            System.Console.WriteLine(csPasswordManager.Instance.Password2);
+
+            //some other part of your code
+            System.Console.WriteLine(csPasswordManager.Instance.Password1);
+            System.Console.WriteLine(csPasswordManager.Instance.Password2);
+
+            //some other part of your code
+            System.Console.WriteLine(csPasswordManager.Instance.Password1);
+            System.Console.WriteLine(csPasswordManager.Instance.Password2);
+
+            //some other part of your code
+            System.Console.WriteLine(csPasswordManager.Instance.Password1);
+            System.Console.WriteLine(csPasswordManager.Instance.Password2);
+
+            //some other part of your code
+            System.Console.WriteLine(csPasswordManager.Instance.Password1);
+            System.Console.WriteLine(csPasswordManager.Instance.Password2);
+            //Maybe your code needs the passwords 100 times
+        }
+    }
+}
---
+++ M3_08_05a/csPasswordManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BOOPM3_08_05a
+{
+    public class csPasswordManager
+    {
+		string _password1 = null;
+		public string Password1 => _password1;
+
+		string _password2 = null;
+		public string Password2 => _password2;
+
+		static csPasswordManager _singleton = null;
+        public csPasswordManager ()
+        {
+			//Connecting to Azure Key Vault to read the passwords.
+			//It takes time, which I simulate with 10sec delay
+			Thread.Sleep(5000);
+			_password1 = "Bullibompa1";
+			_password2 = "MupparnasJulmiddag1";
+		}
+
+		public static csPasswordManager Instance { get
+        {
+			if (_singleton == null)
+            {
+				_singleton = new csPasswordManager();
+            }
+			return _singleton;
+        }}
+	}
+}
